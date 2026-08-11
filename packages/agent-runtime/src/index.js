f83b45f2219006f13176b10622e2b092f8cb5bfd
Wilberry <53071491+Wilberry@users.noreply.github.com>
2026-08-11 13:17:32 +0100

    chore(runtime): remove stale compiled source artifacts

--- packages/agent-runtime/src/index.js
+++
@@ -1,7 +0,0 @@
-export * from './runAgent';
-export * from './embeddings';
-export * from './memory';
-export * from './tools';
-export { callOpenAI, runMultiAgentWorkflow } from './agentRouter';
-export * from './queue';
-export * from './worker';
